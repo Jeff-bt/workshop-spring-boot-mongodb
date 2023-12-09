89dcab2ef8b11b7d263f9bb6cff712c1c6fc3681
Jeff <Jeffjunio.bt@gmail.com>
2023-12-09 19:32:33 -0300

    Consulta simples com query methods

--- src/main/java/com/jeff/workshopmongo/resources/PostResource.java
+++ src/main/java/com/jeff/workshopmongo/resources/PostResource.java
@@ -1,24 +1,17 @@
 package com.jeff.workshopmongo.resources;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jeff.workshopmongo.domain.Post;
-import com.jeff.workshopmongo.domain.User;
-import com.jeff.workshopmongo.dto.UserDTO;
+import com.jeff.workshopmongo.resources.util.URL;
 import com.jeff.workshopmongo.services.PostService;
 
 @RestController
@@ -32,6 +25,13 @@ public class PostResource {
  	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping(value = "/titlesearch")
+ 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
